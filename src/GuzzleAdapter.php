2bedd4f2209425b5b0f88a1ef7e0db26feb30a54
chadicus <chadwickgray@gmail.com>
2016-08-18 15:43:57 -0400

    Update to use Guzzle 6

--- src/GuzzleAdapter.php
+++ src/GuzzleAdapter.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace DominionEnterprises\Api;
+
+use ArrayObject;
 use DominionEnterprises\Util;
-use Guzzle\Http\Client as GuzzleClient;
-use Guzzle\Http\Exception\BadResponseException;
-use Guzzle\Http\Exception\MultiTransferException;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Promise;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Concrete implentation of Adapter interface
@@ -12,17 +15,23 @@ use Guzzle\Http\Exception\MultiTransferException;
 final class GuzzleAdapter implements Adapter
 {
     /**
-     * @var array array of \Guzzle\Http\Message\RequestInterface with keys matching what was given from start()
+     * Collection of Promise\PromiseInterface instances with keys matching what was given from start().
+     *
+     * @var array
      */
-    private $_requests = [];
+    private $_promises = [];
 
     /**
-     * @var array array of \Guzzle\Http\Message\Response with keys matching what was given from start()
+     * Collection of Api\Response with keys matching what was given from start().
+     *
+     * @var array
      */
     private $_responses = [];
 
     /**
-     * @var array array of \Exception with keys matching what was given from start()
+     * Collection of \Exception with keys matching what was given from start().
+     *
+     * @var array
      */
     private $_exceptions = [];
 
@@ -33,7 +42,12 @@ final class GuzzleAdapter implements Adapter
 
     public function __construct()
     {
-        $this->_client = new GuzzleClient();
+        $this->_client = new GuzzleClient(
+            [
+                'allow_redirects' => false, //stop guzzle from following redirects
+                'http_errors' => false, //only for 400/500 error codes, actual exceptions can still happen
+            ]
+        );
     }
 
     /**
@@ -41,18 +55,17 @@ final class GuzzleAdapter implements Adapter
      */
     public function start(Request $request)
     {
-        $this->_requests[] = $this->_client->createRequest(
+        $this->_promises[] = $this->_client->requestAsync(
             $request->getMethod(),
             $request->getUrl(),
-            $request->getHeaders(),
-            $request->getBody(),
-            //stop guzzle from following redirects
-            //only for 400/500 error codes, actual exceptions can still happen
-            ['allow_redirects' => false, 'exceptions' => false]
+            [
+                'headers' => $request->getHeaders(),
+                'body' => $request->getBody(),
+            ]
         );
 
-        end($this->_requests);
-        return key($this->_requests);
+        end($this->_promises);
+        return key($this->_promises);
     }
 
     /**
@@ -64,30 +77,28 @@ final class GuzzleAdapter implements Adapter
     {
         Util::throwIfNotType(['int' => [$endHandle]]);
 
-        if (!empty($this->_requests)) {
-            $multiException = null;
+        $results = $this->fulfillPromises($this->_promises, $this->_exceptions);
+        foreach ($results as $handle => $response) {
             try {
-                $this->_client->send($this->_requests);
-            } catch (MultiTransferException $e) {
-                $multiException = $e;
-            }
-
-            foreach ($this->_requests as $handle => $request) {
-                $response = $request->getResponse();
-                if ($response === null) {
-                    $this->_exceptions[$handle] = $multiException->getExceptionForFailedRequest($request);
-                } else {
-                    try {
-                        $response->json();
-                        $this->_responses[$handle] = $response;
-                    } catch (\Exception $e) {
-                        $this->_exceptions[$handle] = BadResponseException::factory($request, $response);
-                    }
+                $body = []; //default to empty body
+                $contents = (string)$response->getBody();
+                if (trim($contents) !== '') {
+                    $body = json_decode($contents, true);
+                    Util::ensure(
+                        JSON_ERROR_NONE,
+                        json_last_error(),
+                        '\UnexpectedValueException',
+                        [json_last_error_msg()]
+                    );
                 }
-            }
 
-            $this->_requests = [];
+                $this->_responses[$handle] = new Response($response->getStatusCode(), $response->getHeaders(), $body);
+            } catch (\Exception $e) {
+                $this->_exceptions[$handle] = $e;
+            }
         }
+
+        $this->_promises = [];
 
         if (array_key_exists($endHandle, $this->_exceptions)) {
             $exception = $this->_exceptions[$endHandle];
@@ -98,9 +109,37 @@ final class GuzzleAdapter implements Adapter
         if (array_key_exists($endHandle, $this->_responses)) {
             $response = $this->_responses[$endHandle];
             unset($this->_responses[$endHandle]);
-            return new Response($response->getStatusCode(), $response->getHeaders()->toArray(), $response->json());
+            return $response;
         }
 
         throw new \InvalidArgumentException('$endHandle not found');
+    }
+
+    /**
+     * Helper method to execute all guzzle promises.
+     *
+     * @param array $promises
+     * @param array $exceptions
+     *
+     * @return array Array of fulfilled PSR7 responses.
+     */
+    private function fulfillPromises(array $promises, array &$exceptions)
+    {
+        if (empty($promises)) {
+            return [];
+        }
+
+        $results = [];
+        Promise\each(
+            $this->_promises,
+            function (ResponseInterface $response, $index) use (&$results) {
+                $results[$index] = $response;
+            },
+            function (RequestException $e, $index) use (&$exceptions) {
+                $exceptions[$index] = $e;
+            }
+        )->wait();
+
+        return $results;
     }
 }
