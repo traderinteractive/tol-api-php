743fa30bd69c6e43bf8fe62f2d35d859e603529f
chadicus <chadwickgray@gmail.com>
2017-03-23 12:22:50 -0400

    Make request public in test DeleteAdapter

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -715,29 +715,29 @@ final class PostAdapter implements Adapter
 
 final class DeleteAdapter implements Adapter
 {
-    private $_request;
+    public $request;
 
     public function start(Request $request)
     {
-        $this->_request = $request;
+        $this->request = $request;
     }
 
     public function end($handle)
     {
-        if (substr($this->_request->getUrl(), -5) === 'token') {
+        if (substr($this->request->getUrl(), -5) === 'token') {
             return new Response(200, ['Content-Type' => ['application/json']], ['access_token' => 'a token', 'expires_in' => 1]);
         }
 
         if (
-            $this->_request->getMethod() === 'DELETE' &&
-            $this->_request->getUrl() === 'baseUrl/v1/resource+name/the+id' &&
-            $this->_request->getHeaders() === [
+            $this->request->getMethod() === 'DELETE' &&
+            $this->request->getUrl() === 'baseUrl/v1/resource+name/the+id' &&
+            $this->request->getHeaders() === [
                 'Content-Type' => 'application/json',
                 'Accept-Encoding' => 'gzip',
                 'Authorization' => 'Bearer a token',
             ]
         ) {
-            $body = $this->_request->getBody();
+            $body = $this->request->getBody();
 
             if ($body === null || $body === '{"the key":"the value"}') {
                 return new Response(204, ['Content-Type' => ['application/json']]);
